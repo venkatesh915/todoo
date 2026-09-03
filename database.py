16dbeca52be16f572c4aed4cd906f3ee110f6ffc
Boddu Venkateswara Rao <venkateshboddu923@gmail.com>
2026-09-03 10:19:15 +0530

    initial_commit

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres.lknlffqsjbqggovvadwj:Venkatesh.com123@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres"
-
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
